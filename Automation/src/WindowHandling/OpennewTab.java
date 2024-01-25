47de2635a61a5d2c1c18ef2d38e8271317b88d31
SaurabhTapkir7744 <saurabhtapkir44@gmail.com>
2024-01-25 12:31:31 +0530

    New Tab Created

---
+++ Automation/src/WindowHandling/OpennewTab.java
@@ -0,0 +1,21 @@
+package WindowHandling;
+
+import java.time.Duration;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WindowType;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class OpennewTab {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.flipkart.com/");
+//open new tab
+		driver.switchTo().newWindow(WindowType.TAB);
+	}
+
+}
