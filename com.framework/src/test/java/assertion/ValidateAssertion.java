fdc20ca2f8c2301376e2fa58cae043083fc7b5ae
Saurabh Tapkir <saurabhtapkir44@gmail.com>
2024-03-04 00:56:57 -0800

    framework

--- com.framework/src/test/java/assertion/ValidateAssertion.java
+++ com.framework/src/test/java/assertion/ValidateAssertion.java
@@ -2,6 +2,7 @@ package assertion;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.Assert;
 import org.testng.annotations.Test;
 
 public class ValidateAssertion {
@@ -9,11 +10,13 @@ public class ValidateAssertion {
 	public void title() {
 		WebDriver driver = new ChromeDriver();
 		driver.get("http://desktop-8fphgbh/login.do");
-		if (driver.getTitle().equals("actiTIME - Login")) {
-			System.out.println("correct");
-		} else {
-			System.out.println("Reject");
-		}
+		//if (driver.getTitle().equals("actiTIME - Login")) 
+//		{
+//			System.out.println("correct");
+//		} else {
+//			System.out.println("Reject");
+//		}
+		Assert.assertEquals(driver.getTitle(), "actiTIME - Logi");
 
 		String title = driver.getTitle();
 		System.out.println(title);
--- com.framework/src/test/java/pomClass/StaleElementReferenceException.java
+++ com.framework/src/test/java/pomClass/StaleElementReferenceException.java
@@ -1,5 +1,27 @@
 package pomClass;
 
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.chrome.ChromeDriver;
+
 public class StaleElementReferenceException {
 
+	public static void main(String[] args) throws InterruptedException {
+		WebDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(20));
+		driver.get("http://desktop-8fphgbh/login.do");
+
+		WebElement username = driver.findElement(By.name("username"));
+		username.sendKeys("admin");
+
+		Thread.sleep(2000);
+		driver.navigate().refresh();
+		WebElement username2 = driver.findElement(By.name("username"));
+		username2.sendKeys("admin2");
+
+	}
 }
